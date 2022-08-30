491a7ff638db468c32671569e7f8a8f4f8fb3ac0
saitadikonda99 <saiitadikonda7@gmail.com>
2022-08-30 23:06:47 +0530

    Playing With Characters

---
+++ char.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+char ch,k,s[100],sen[100];
+scanf("%c\n",&ch);
+scanf("%s\n",s);
+scanf("%[^\n]",sen);
+
+printf("%c\n%s\n%s\n",ch,s,sen);
+    return 0;
+}
+
